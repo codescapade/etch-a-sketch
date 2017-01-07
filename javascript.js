fa44358d0fa42c65f12346c68c8c0db05be08b3d
Jurien Meerlo <codescapade@gmail.com>
2017-01-07 16:05:41 +0000

    finished the etch a sketch

--- javascript.js
+++ javascript.js
@@ -1,27 +1,61 @@
 
+var startColor = { red: 60, green: 200, blue: 230 };
+var whiteColor = {red: 238, green: 238, blue: 238 };
+
 $(document).ready(function ()
 {
+	createGrid(16);
+});
 
-	var width = 16;
-	var height = 16;
+function resetGrid ()
+{
+	// show the prompt to enter the number of tiles per side
+	var size = prompt("Number of squares per side?");
+	createGrid(size);
+}
+
+function createGrid (size)
+{
+	// clear the grid div
+	$('#grid').empty();
+
+	// create the content for the grid depending on the size
 	var content = '';
-	
-	for (var y = 0; y < height; y++)
+	for (var y = 0; y < size; y++)
 	{
-		for (var x = 0; x < width; x++)
+		for (var x = 0; x < size; x++)
 		{
 			content += '<div class="tile"></div>';
 		}
-		content += '<br>';
 	}
 	$('#grid').append(content);
+
+	// set the grid div size based on the div width and height
 	var gridWidth = $('#grid').width();
 	var gridHeight = $('#grid').height();
-	$('.tile').width(gridWidth / width);
-	$('.tile').height(gridHeight / height);
+	$('.tile').width(gridWidth / size);
+	$('.tile').height(gridHeight / size);
+
+	// set the color of each tile to a light grey
+	$('.tile').css('background-color', 'rgb(' + whiteColor.red + ', ' + whiteColor.green + ', ' + whiteColor.blue + ')');
 
 	$('.tile').mouseenter(function ()
 	{
-		$(this).css('background-color', '#55aadd');
+		var color = $(this).css('background-color');
+		var rgb = color.slice(4, color.indexOf(")")).split(",");
+
+		var red = parseInt(rgb[0],10);
+  		var green = parseInt(rgb[1],10);
+  		var blue = parseInt(rgb[1], 10);
+
+  		// if the tile is already colored decrease the values to make it darker
+		if (red !== whiteColor.red)
+		{
+			$(this).css('background-color', 'rgb(' + (red - 25) + ', ' + (green - 25) + ', ' + (blue - 25) + ')');
+		}
+		else // set the first color value
+		{
+			$(this).css('background-color', 'rgb(' + startColor.red + ', ' + startColor.green + ', ' + startColor.blue + ')');
+		}
 	});
-});
+}
